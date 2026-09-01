a1618120a900bf8f7d5c490b99985b4e19549e73
eoliveira-ship-it <eoliveira@grouphh.com>
2026-09-01 21:48:26 +0100

    Add files via upload

---
+++ CRRCORP_Split.bat
@@ -0,0 +1,8 @@
+findstr "\<M............C1\>" CRRCORP.dat > CRRCORP_C1.txt
+findstr "\<M............F1\>" CRRCORP.dat > CRRCORP_F1.txt
+findstr "\<M............F2\>" CRRCORP.dat > CRRCORP_F2.txt
+findstr "\<M............M1\>" CRRCORP.dat > CRRCORP_M1.txt
+findstr "\<M............P1\>" CRRCORP.dat > CRRCORP_P1.txt
+findstr "\<M............P2\>" CRRCORP.dat > CRRCORP_P2.txt
+findstr "\<M............P9\>" CRRCORP.dat > CRRCORP_P9.txt
+Pause
