c196ae030ce82e16079c43aedf785008185bd94f
eoliveira-ship-it <eoliveira@grouphh.com>
2026-09-02 14:56:07 +0100

    Delete CRRCORP_Split_vPACT.bat

--- CRRCORP_Split_vPACT.bat
+++
@@ -1,8 +0,0 @@
-findstr "\<M............C1\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_C1.txt
-findstr "\<M............F1\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_F1.txt
-findstr "\<M............F2\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_F2.txt
-findstr "\<M............M1\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_M1.txt
-findstr "\<M............P1\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_P1.txt
-findstr "\<M............P2\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_P2.txt
-findstr "\<M............P9\>" CRRCORP_vPACT.dat > CRRCORP_vPACT_P9.txt
-Pause
